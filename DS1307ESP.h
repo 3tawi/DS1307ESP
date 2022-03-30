e0f5d7a38aad6d209dec40a640ad59d72e40ce4a
3tawi <96850409+3tawi@users.noreply.github.com>
2022-03-30 02:02:01 +0000

    Update DS1307ESP.h

--- DS1307ESP.h
+++ DS1307ESP.h
@@ -1,4 +1,7 @@
 
+//An Arduino library for setting and retrieving RTC DS1307 on ESP32 ESP8266 ESP01 boards
+//Testing of the ESP32 ESP8266 ESP8266–01 ESP 01 module by DS1307ESP library https://youtu.be/s3upvC23QGY
+
 
 #ifndef DS1307ESP_H
 #define DS1307ESP_H
