137261d6dc38e8575ba715a8c052545d5b3eb29e
3tawi <96850409+3tawi@users.noreply.github.com>
2022-03-30 02:01:37 +0000

    Update DS1307ESP.cpp

--- DS1307ESP.cpp
+++ DS1307ESP.cpp
@@ -1,7 +1,9 @@
+//An Arduino library for setting and retrieving RTC DS1307 on ESP32 ESP8266 ESP01 boards
+//Testing of the ESP32 ESP8266 ESP8266–01 ESP 01 module by DS1307ESP library https://youtu.be/s3upvC23QGY
+
 
 #include "DS1307ESP.h"
 #include <Wire.h>
-
 
 
 uint16_t DS1307ESP::yeardays(uint16_t y, uint8_t m, uint8_t d) {
